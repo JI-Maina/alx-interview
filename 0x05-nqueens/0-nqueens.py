23bb74f742204ca9b74893615b914af76b0531a3
JI-Maina <irungujmaina@gmail.com>
2023-08-17 10:47:29 +0300

    N queens

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -9,16 +9,48 @@ if len(sys.argv) <= 1:
 
 
 try:
-    int(sys.argv[1])
+    n_q = int(sys.argv[1])
 except ValueError:
     print("N must be a number")
     sys.exit(1)
 
-if int(sys.argv[1]) < 4:
+if n_q < 4:
     print("N must be at least 4")
     sys.exit(1)
 
-n = int(sys.argv[1])
-board = [[i, j] for i in range(n) for j in range(n)]
 
-print(board)
+def solve_nqueens(n):
+    """solves the N queens problem, of placing N non-attacking queens
+    on an N×N chessboard"""
+
+    if n == 0:
+        return [[]]
+
+    inner_solution = solve_nqueens(n - 1)
+    return [solution + [(n, i + 1)]
+            for i in range(n_q)
+            for solution in inner_solution
+            if safe_queen((n, i + 1), solution)]
+
+
+def attack_queen(square, queen):
+    """Checks if current sqr attacks the queen"""
+    (row1, col1) = square
+    (row2, col2) = queen
+    return (row1 == row2) or (col1 == col2) or\
+        abs(row1 - row2) == abs(col1 - col2)
+
+
+def safe_queen(sqr, queens):
+    """Checks if queen is safe"""
+    for queen in queens:
+        if attack_queen(sqr, queen):
+            return False
+    return True
+
+
+for answer in reversed(solve_nqueens(n_q)):
+    result = []
+    for p in [list(p) for p in answer]:
+        result.append([i - 1 for i in p])
+    print(result)
