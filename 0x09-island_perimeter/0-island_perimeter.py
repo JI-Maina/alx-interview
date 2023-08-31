fc06fe263925548508ba5052e4bae834fa161861
JI-Maina <irungujmaina@gmail.com>
2023-08-31 17:55:41 +0300

    Island Perimeter

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -6,26 +6,50 @@ perimeter of the island described in grid."""
 def island_perimeter(grid):
     """returns the perimeter of the island described in grid
     """
-    row = len(grid)
-    col = len(grid[0])
-    perimeter = 0
-    
-    for i in range(row):
-        for j in range(col):
-            if grid[i][j] == 0:
-                continue
+    visit = set()
 
-            if i == 0 or grid[i-1][j] == 0:
-                perimeter += 1
+    def dfs(i, j):
+        if i >= len(grid) or j >= len(grid[0]) or \
+                i < 0 or j < 0 or grid[i][j] == 0:
+            return 1
 
-            if j == 0 or grid[i][j-1] == 0:
-                perimeter += 1
+        if (i, j) in visit:
+            return 0
 
-            if i == row - 1 or grid[i+1][j] == 0:
-                perimeter += 1
+        visit.add((i, j))
 
-            if j == col - 1 or grid[i][j+1] == 0:
-                perimeter += 1
-            print(perimeter)
+        perimeter = dfs(i, j + 1)
+        perimeter += dfs(i + 1, j)
+        perimeter += dfs(i, j - 1)
+        perimeter += dfs(i - 1, j)
 
-    return perimeter
+        return perimeter
+
+    for i in range(len(grid)):
+        for j in range(len(grid[0])):
+            if grid[i][j]:
+                return dfs(i, j)
+
+# def island_perimeter(grid):
+#    row = len(grid)
+#   col = len(grid[0])
+#    perimeter = 0
+#    for i in range(row):
+#        for j in range(col):
+#            if grid[i][j] == 0:
+#                continue
+
+#            if i == 0 or grid[i-1][j] == 0:
+#                perimeter += 1
+
+#            if j == 0 or grid[i][j-1] == 0:
+#                perimeter += 1
+
+#            if i == row - 1 or grid[i+1][j] == 0:
+#                perimeter += 1
+
+#            if j == col - 1 or grid[i][j+1] == 0:
+#                perimeter += 1
+#            print(perimeter)
+
+#    return perimeter
